87a73c753c062b0ea7ca7f940a0b12e9da17defe
Moe Pwint Phyu <88941041+Mpphyu@users.noreply.github.com>
2022-03-02 12:22:52 +0630

    Add files via upload
    Alternative Syntax ရေးနည်းလို့ခေါ်ပါတယ်။ တွန့်ကွင်းအဖွင့်အပိတ်တွေ မပါတေ့တာပါ။
    တွန့်ကွင်းအဖွင့်အစား Full Colon သင်္ကေ တကို အသုံးပြုပြီး နောက်ဆုံး တွန့်ကွင်းအပိတ်နေရာမှာ
    endif နဲ့ ပိတ်ပေးလိုက်တာပါ။ တွန့်ကွင်းအဖွင့်အပိတ်တွေ က ရိုးရိုး PHP ကုဒ်ထဲမှာ အဆင်ပြေ ပေမယ့်
    အခုလို HTML နဲ့ ရောရေးတဲ့အခါ ရေးရတာရော ဖတ်ရတာပါ ခက်စေပါတယ်။ ဒါကြောင့် အခုလို
    Alternative Syntax နဲ့ ရေးလိုက်တဲ့အတွက် ရေးရတာ ပိုအဆင်ပြေသွားသလို၊ ဖတ်ရတာလည်း ပိုရှင်း
    သွားမှာ ဖြ စ်ပါတယ်။ ဒီရေးနည်းကို while, for နဲ့ switch Statement တွေ မှာလည်း အသုံးုနိုင်
    ပါတယ်။ Array တွေ ကို Loop ပါတ်ဖို့ သုံးရတဲ့ foreach Statement မှာလည်း အသုံးုနိုင်ပါတယ်။

---
+++ datetime_alterSyn.php
@@ -0,0 +1,11 @@
+<h1> Home Page </h1>
+
+<?php $hour = date('h') ?>
+
+<p>
+    <?php if ($hour <6 || $hour >18) : ?>
+        <b> Night Time </b>
+    <?php else : ?>
+        <i> Day Time </i>
+    <?php endif ?> 
+</p>
