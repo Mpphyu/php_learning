b6124e330c72ada9fa6d24bd626502c6e81d21ca
Moe Pwint Phyu <88941041+Mpphyu@users.noreply.github.com>
2022-03-04 10:32:35 +0630

    Add files via upload
    Array ထဲက တန်ဖိုးတစ်ခုကို ရယူလိုရင်တေ့ သက်ဆိုင်ရာ Index နဲ့ထောက်ပြပြီး ရယူနိုင်ပါတယ်။

---
+++ arrayIndex.php
@@ -0,0 +1,11 @@
+<?php
+
+$users = [
+    ["name" => "Alice" , "age" => 22],
+    ["name" => "Bob", "age" => 23],
+    ["name" => "Tom", "age" => 24],
+];
+
+print_r($users [0]);
+
+echo $users [0] ['name'];
